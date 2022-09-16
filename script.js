4399fb27a320a4b7c7512607c16f14ed1149d2cc
Ciprian Andrei Rosu <cip.rosu@yahoo.com>
2022-09-16 19:11:15 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ options.forEach(option =>{
         optionMenu.classList.remove("active");
     });
 });
+
+
+
+
+const optionMenu2 = document.querySelector(".select-menu2"),
+       selectBtn2 = optionMenu2.querySelector(".select-btn2"),
+       options2 = optionMenu2.querySelectorAll(".option2"),
+       sBtn_text2 = optionMenu2.querySelector(".sBtn-text2");
+
+selectBtn2.addEventListener("click", () => optionMenu2.classList.toggle("active2"));       
+
+options2.forEach(option =>{
+    option.addEventListener("click", ()=>{
+        // let selectedOption = option.querySelector(".option-text").innerText;
+        // sBtn_text.innerText = selectedOption;
+
+        optionMenu2.classList.remove("active2");
+    });
+});
